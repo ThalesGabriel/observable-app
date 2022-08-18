d1a28c538c9e3407396ff2d89ab9ee6abe8fb077
Thales Gabriel <thales.anjos@zenvia.com>
2022-08-18 10:17:49 -0300

    prometheus, grafana and influx

---
+++ src/test/java/com/example/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+
+}
